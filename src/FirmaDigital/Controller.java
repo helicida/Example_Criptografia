f87b0f496d9302ec76d6cd56a219451ced1d7cd3
Sergi Barjola <sergibca@gmail.com>
2016-04-07 15:56:51 +0200

    Añadidos más metodos. La firma de archivos ya funciona correctamente.

--- src/FirmaDigital/Controller.java
+++ src/FirmaDigital/Controller.java
@@ -1,9 +1,13 @@
 package FirmaDigital;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
@@ -13,17 +17,20 @@ import java.security.PrivateKey;
  */
 public class Controller {
 
-    public static final String PRIVATE_KEY_FILE = "private.key";    // private.key
-    public static final String FITXER_PLA = "Sparring.pdf";         // PDF normal
-    public static final String FITXER_SIGNAT = "firmat.pdf";        // PDF firmado
+    public static final String PRIVATE_KEY_FILE = "/home/46465442z/encriptar/private.key";      // private.key
+    public static final String FITXER_PLA = "/home/46465442z/encriptar/Pt13_Mireia_Fer.java";   // Archivo normal
+    public static final String FITXER_SIGNAT = "/home/46465442z/encriptar/FIRMADO.java";        // Archivo firmado
 
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, ClassNotFoundException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchPaddingException {
 
+        // Llaves
         KeyPair keyPair = null;
         PrivateKey prik = null;
 
+        // Archivo
         File f = new File(FITXER_PLA);
 
+        // Comprueba si las llaves están generadas y si no las crea
         if(!Utils.areKeysPresent()){
             keyPair = Utils.generateKey();
             prik = keyPair.getPrivate();
@@ -33,8 +40,11 @@ public class Controller {
             prik = (PrivateKey) inputStream.readObject();
         }
 
+        // Arrays
         byte[] digestionat =  Utils.digestiona(f,"MD5");
         byte[] encryptDigestionat = Utils.signar(digestionat, prik);
+
+        // Imprimimos la información por la pantalla y escribimos el archivo con firma
         System.out.println("Longitud del fitxer: " + f.length());
         System.out.println("Longitud de la firma: " + encryptDigestionat.length);
         Utils.write(FITXER_SIGNAT, Utils.concatenateByteArrays(Utils.read(f), encryptDigestionat));
--- src/FirmaDigital/Utils.java
+++ src/FirmaDigital/Utils.java
@@ -1,6 +1,11 @@
 package FirmaDigital;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -36,30 +41,52 @@ public class Utils {
         return digest;
     }
 
-    public static byte[] signar(byte[] array, PrivateKey privateKey){
+    // Firmar un archivo
+    public static byte[] signar(byte[] array, PrivateKey privateKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
 
         byte[]encryptDigestionat = null;
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+        encryptDigestionat =  cipher.doFinal(array);
 
         return encryptDigestionat;
     }
 
-    public static void write(File file, byte[] arrayBytes){
+    // desencripta un archivo
+    public static byte[] decrypt(byte[] encrypted, PublicKey publicKey) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException {
 
+        byte[]decryptDigestionat = null;
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, publicKey);
+        decryptDigestionat = cipher.doFinal(encrypted);
 
-
+        return decryptDigestionat;
     }
 
+    // Escribimos un archivo
+    public static void write(String ruta, byte[] arrayBytes) throws IOException {
+
+        FileOutputStream fos = new FileOutputStream(ruta);
+        fos.write(arrayBytes);
+        fos.close();
+    }
+
+    // Generamos la key
     public static KeyPair generateKey() throws NoSuchAlgorithmException {
 
         kpg = KeyPairGenerator.getInstance("RSA");
-        kpg.initialize(2048);
+        kpg.initialize(1024);
         KeyPair kp = kpg.genKeyPair();
         return kp;
     }
 
-    public static byte[] concatenateByteArrays(byte[] encryptDigestionar){
+    // Concatenamos los dos byte
+    public static byte[] concatenateByteArrays(byte[] array1, byte[] array2){
 
-        byte[]encryptDigestionat = null;
+        // Concatenamos los arrays
+        byte[]encryptDigestionat = new byte[array1.length + array2.length];
+        System.arraycopy(array1, 0, encryptDigestionat, 0, array1.length);
+        System.arraycopy(array2, 0, encryptDigestionat, array1.length, array2.length);
 
         return encryptDigestionat;
     }
@@ -72,5 +99,12 @@ public class Utils {
         fileToBytes = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
 
         return fileToBytes;
+    }
+
+    public static byte[] extraerHash(File file, String algoritme) throws NoSuchAlgorithmException, IOException {
+
+        MessageDigest messageDigest = MessageDigest.getInstance(algoritme);
+        messageDigest.update(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
+        return messageDigest.digest();
     }
 }
